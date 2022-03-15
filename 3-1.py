2d49371c7643e1c6f31a10f2e6718790ad61932a
Isaac Telloian <iketelloian@fresnostate.edu>
2022-03-15 14:30:15 -0700

    commit

--- 3-1.py
+++
@@ -1,7 +0,0 @@
-'''
-notes there are time you need to add, remove, and check connectitvity to program
-use; "git origin remove" to remove/destroy conection
-use; git add origin http:..." to add connection
-use: git remote remove origin
-'''
-# can use git checkout -b "branch name" to create new branch
