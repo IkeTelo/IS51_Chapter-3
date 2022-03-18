10248a23f7d4b25b4f5b8bf2bc368824f2284ea9
Isaac Telloian <iketelloian@fresnostate.edu>
2022-03-17 20:11:36 -0700

    Commit

---
+++ 3-1.py
@@ -0,0 +1,56 @@
+"Relation and Logical Operatiors"
+# Condition is exoression 
+#   - Involving relational operators (such as < and > =)
+#   - Logical operatiors (i.e. and, or, and not)
+#   - Evaluates to either True or False
+#  Conditions used to make decicions"
+#   - control loops
+#   - choose between options
+
+"ASCII Values"
+# ASCII values determin order used to compare strings with relational operators
+# Associated with keyboard letters, charachters, numerals
+#   - ASCII values are numbers ranging from 32 to 126
+#  A few ASCII values
+# 32 = space, 33 = !, 34 = ", 35 = #, 48 = 0, 49 = 1, 57 = 9, 65 = A, 66 = B, 90 = Z, 97 = a, 98 = b, 122 = z, 123 = (, 125 = ), 126 = ~
+
+"""
+Relational Operator
+<, <=, >, >=, !=, == (less than, less or equal than, greater than, greater or equal than, not equal to, equal to)
+Logical Operator
+and, or, not
+"""
+#  Relational operatior less than (<) can be applied to
+#   - numbers, strings, and other objects
+#  for strings, the ASCII table determines order of charachters
+# in = substring of, and no in = not a substring of
+
+#  some rules
+#   - an int can be compared to a float, otherwise values of diffrent types cannot be compared, relational operatora can be applied to list or tuples
+
+# true_or_false = "b" > "a"; #True
+# true_or_false = "z" < "!"; #True
+# true_or_false = "z" < "!" and "b" > "a"; #False
+# true_or_false = "z" < "!" or "b" > "c"; #True
+
+
+# C = "abc"
+# C < "good" + "d"
+# true_or_false = C < "good" + "d"
+
+# true_or_false = 3 in [3, 8]
+# true_or_false = "b" > "a"
+# true_or_false = "z" < "!"
+# print("true_or_fasle", true_or_false)
+
+
+# list1 = [6, 4, -5, 3.5]
+# print(list1)
+# list1.sort()
+# print(list1)
+
+# list2 = ["ha", "hi", "b", "7"]
+# print(list2)
+# list2.sort()
+# print(list2)
+
